c6feaf3576238f5fbd1c3656c0c86b5086741c06
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2024-12-14 13:41:26 +0530

    Update works.php

--- users/user_1/apps/works/works.php
+++ users/user_1/apps/works/works.php
@@ -66,7 +66,8 @@
         <center>
         <div id="add_job" class="animate__animated animate__slideInDown">
             <h1>Add New Job</h1><br>
-            <form id="add_work" name="submit-to-google-sheet">
+            <form id="add_work" action="#" method="POST">
+            <!-- <form id="add_work" name="submit-to-google-sheet" method="POST"> -->
                 <p id="add">Your Name</p>
                 <input type="text" name="rname" placeholder="Type Your Name" id="ridername" required="required"></input>
                 <p id="add">Sender Name</p>
@@ -98,18 +99,26 @@ include '../../../../connection.php';
 
 <?php
 
-if(isset($_POST['submit']))
+if($_POST['submit'])
 {
-    $rname=$_GET['rname'];
-    $sname=$_GET['sender'];
-    $date=$_GET['date'];
-    $work=$_GET['work'];
-    $from=$_GET['from'];
-    $to=$_GET['to'];
-    $km=$_GET['km'];
-    $query="INSERT INTO rides(rname,sender,date,work,from,to,km) VALUES ('$rname','$sname','$date','$work','$from','$to','$km')";
+    $rname=$_POST['rname'];
+    $sname=$_POST['sender'];
+    $date=$_POST['date'];
+    $work=$_POST['work'];
+    $from=$_POST['from'];
+    $to=$_POST['to'];
+    $km=$_POST['km'];
+
+    $query="INSERT INTO data values ('$rname','$sname','$date','$work','$from','$to','$km')";
     $send=mysqli_query($conn,$query);
 
+    if($send)
+    {
+        echo "Data inserted into database";
+    }
+    else{
+        echo "Failed";
+    }
 }
 
 
