b162d9d738786259125a8571075bc10755859bc1
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2024-12-14 14:40:40 +0530

    Create action.php

---
+++ users/user_1/apps/action.php
@@ -0,0 +1,32 @@
+<?php
+include '../../../connection.php';
+?>
+
+
+<?php
+
+if(isset($_POST['submit']))
+{
+    $rname=$_POST['rname'];
+    $sname=$_POST['sender'];
+    $date=$_POST['date'];
+    $work=$_POST['work'];
+    $from=$_POST['from'];
+    $to=$_POST['to'];
+    $km=$_POST['km'];
+
+    $query="INSERT INTO data values ('$rname','$sname','$date','$work','$from','$to','$km')";
+    $send=mysqli_query($conn,$query);
+
+    if($send)
+    {
+        echo "Data inserted into database";
+        header("Location: works/works.php");
+    }
+    else{
+        echo "Failed";
+    }
+}
+
+
+?>
