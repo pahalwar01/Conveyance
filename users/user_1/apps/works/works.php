bb4836843363aa8a24145f61a90b9407ea4cfafe
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2024-12-14 14:40:43 +0530

    Update works.php

--- users/user_1/apps/works/works.php
+++ users/user_1/apps/works/works.php
@@ -66,7 +66,7 @@
         <center>
         <div id="add_job" class="animate__animated animate__slideInDown">
             <h1>Add New Job</h1><br>
-            <form id="add_work" action="works.php" method="POST">
+            <form id="add_work" action="../action.php" method="POST">
             <!-- <form id="add_work" name="submit-to-google-sheet" method="POST"> -->
                 <p id="add">Your Name</p>
                 <input type="text" name="rname" placeholder="Type Your Name" id="ridername" required="required"></input>
@@ -97,33 +97,7 @@
 include '../../../../connection.php';
 ?>
 
-<?php
 
-if(isset($_POST['submit']))
-{
-    $rname=$_POST['rname'];
-    $sname=$_POST['sender'];
-    $date=$_POST['date'];
-    $work=$_POST['work'];
-    $from=$_POST['from'];
-    $to=$_POST['to'];
-    $km=$_POST['km'];
-
-    $query="INSERT INTO data values ('$rname','$sname','$date','$work','$from','$to','$km')";
-    $send=mysqli_query($conn,$query);
-
-    if($send)
-    {
-        echo "Data inserted into database";
-        // header("Location: works.php");
-    }
-    else{
-        echo "Failed";
-    }
-}
-
-
-?>
 
 <script type="module">
 
